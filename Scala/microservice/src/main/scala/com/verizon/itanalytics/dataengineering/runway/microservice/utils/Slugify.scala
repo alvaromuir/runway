741a185ecfc44c75cc64d4c000ac34d33459bddf
Muir, Alvaro <alvaro.muir@one.verizon.com>
2018-04-09 18:31:19 -0400

    added newline strip

--- Scala/microservice/src/main/scala/com/verizon/itanalytics/dataengineering/runway/microservice/utils/Slugify.scala
+++ Scala/microservice/src/main/scala/com/verizon/itanalytics/dataengineering/runway/microservice/utils/Slugify.scala
@@ -10,6 +10,7 @@ object Slugify {
     Normalizer.normalize(input, Normalizer.Form.NFD)
       .replaceAll("[^\\w\\s-]", "") // Remove all non-word, non-space or non-dash characters
       .replace('-', ' ')            // Replace dashes with spaces
+      .replace("\n", "").replace("\r", "") // remove newline characters
       .trim                         // Trim leading/trailing whitespace (including what used to be leading/trailing dashes)
       .replaceAll("\\s+", "-")      // Replace whitespace (including newlines and repetitions) with single dashes
       .toLowerCase                  // Lowercase the final results
