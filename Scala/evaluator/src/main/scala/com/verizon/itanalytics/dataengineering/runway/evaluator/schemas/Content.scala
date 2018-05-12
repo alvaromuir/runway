67a0a9dee8813dc4bce644ffdf62cdf7ad52203c
Muir, Alvaro <alvaro.muir@one.verizon.com>
2018-05-12 14:14:16 -0400

    abstracted content from bayesian network

---
+++ Scala/evaluator/src/main/scala/com/verizon/itanalytics/dataengineering/runway/evaluator/schemas/Content.scala
@@ -0,0 +1,17 @@
+package com.verizon.itanalytics.dataengineering.runway.evaluator.schemas
+
+/*
+* Project: Runway
+* Alvaro Muir, Verizon IT Analytics: Data Engineering
+* 05 12, 2018
+*/
+
+
+trait Content {
+  case class Content(locator: Locator)
+
+  case class Locator(publicId: Option[String] = None,
+                     systemId: Option[String] = None,
+                     lineNumber: Int = 1,
+                     columnNumber: Int = 1)
+}
