66e5a147c81ed97a7319a2bcaa3fc4e8da236908
Muir, Alvaro <alvaro.muir@one.verizon.com>
2018-05-12 11:05:40 -0400

    Updated Expression and Taxonomy schemas

--- Scala/evaluator/src/main/scala/com/verizon/itanalytics/dataengineering/runway/evaluator/schemas/Expression.scala
+++ Scala/evaluator/src/main/scala/com/verizon/itanalytics/dataengineering/runway/evaluator/schemas/Expression.scala
@@ -1,12 +1,118 @@
 package com.verizon.itanalytics.dataengineering.runway.evaluator.schemas
 
 /*
-* Project: Runway
-* Alvaro Muir, Verizon IT Analytics: Data Engineering
-* 04 30, 2018
-*/
+ * Project: Runway
+ * Alvaro Muir, Verizon IT Analytics: Data Engineering
+ * 04 30, 2018
+ */
 
+trait Expression extends Taxonomy {
 
-trait Expression {
+  case class Expression(
+      constant: Option[Constant] = None,
+      fieldRef: Option[FieldRef] = None,
+      normContinuous: Option[NormContinuous] = None,
+      normDiscrete: Option[NormDiscrete] = None,
+      discretize: Option[Discretize] = None,
+      mapValues: Option[MapValues] = None,
+      textIndex: Option[TextIndex] = None,
+      apply: Option[Apply] = None,
+      aggregate: Option[Aggregate] = None,
+      lag: Option[Lag] = None
+  )
+
+  case class Constant(
+      dataType: DataType
+  )
+
+  case class FieldRef(
+      extension: Option[Seq[Extension]] = None,
+      field: String,
+      mapMissingTo: Option[String] = None
+  )
+
+  case class NormContinuous(extension: Option[Seq[Extension]] = None,
+                            linearNorm: Seq[LinearNorm],
+                            mapMissingTo: Double,
+                            field: String,
+                            outlierTreatmentMethod: String = "asIs")
+
+  case class LinearNorm(
+      extension: Option[Seq[Extension]] = None,
+      orig: Double,
+      norm: Double
+  )
+
+  case class NormDiscrete(extension: Option[Seq[Extension]] = None,
+                          field: String,
+                          value: String,
+                          mapMissingTo: Double)
+
+  case class Discretize(extension: Option[Seq[Extension]] = None,
+                        discretizeBin: Option[DiscretizeBin] = None,
+                        field: String,
+                        mapMissingTo: Option[String] = None,
+                        defaultValue: Option[String] = None,
+                        dataType: DataType)
+
+  case class DiscretizeBin(extension: Option[Seq[Extension]] = None,
+                           binValue: String)
+
+  case class MapValues(extension: Option[Seq[Extension]] = None,
+                       fieldColumnPair: Option[Seq[FieldColumnPair]] = None,
+                       tableLocator: Option[TableLocator] = None,
+                       inlineTable: Option[InlineTable] = None,
+                       mapMissingTo: Option[String] = None,
+                       defaultValue: Option[String] = None,
+                       outputColumn: String,
+                       dataType: DataType)
+
+  case class FieldColumnPair(extension: Option[Seq[Extension]] = None,
+                             field: String,
+                             column: String)
+
+  case class TextIndex(
+      extension: Option[Seq[Extension]] = None,
+      textIndexNormalization: Option[Seq[TextIndexNormalization]] = None,
+      textField: String,
+      localTermWeights: String = "termFrequency",
+      isCaseSensitive: Boolean = false,
+      maxLevenshteinDistance: Int = 0,
+      countHits: String = "allHits",
+      wordSeparatorCharacterRE: String = "\\s",
+      tokenize: Boolean = true)
+
+  case class TextIndexNormalization(extension: Option[Seq[Extension]] = None,
+                                    tableLocator: Option[TableLocator] = None,
+                                    inlineTable: Option[InlineTable] = None,
+                                    infield: String = "string",
+                                    outField: String = "stem",
+                                    regexField: String = "regex",
+                                    recursive: Boolean = false,
+                                    isCaseSensitive: Boolean,
+                                    maxLevenshteinDistance: Int,
+                                    wordSeparatorCharacterRE: String,
+                                    tokenize: Boolean)
+
+  case class Apply(extension: Option[Seq[Extension]] = None,
+                   function: String,
+                   mapMissingTo: Option[String] = None,
+                   defaultValue: Option[String] = None,
+                   invalidValueTreatment: String = "returnInvalid")
+
+  case class Aggregate(extension: Option[Seq[Extension]] = None,
+                       field: String,
+                       function: String,
+                       groupField: Option[String] = None,
+                       sqlWhere: Option[String] = None)
+
+  case class Lag(extension: Option[Seq[Extension]] = None,
+                 blockIndicator: Option[Seq[BlockIndicator]] = None,
+                 field: String,
+                 n: Int = 1)
+
+  case class BlockIndicator(
+      field: String
+  )
 
 }
--- Scala/evaluator/src/main/scala/com/verizon/itanalytics/dataengineering/runway/evaluator/schemas/Taxonomy.scala
+++ Scala/evaluator/src/main/scala/com/verizon/itanalytics/dataengineering/runway/evaluator/schemas/Taxonomy.scala
@@ -1,25 +1,29 @@
 package com.verizon.itanalytics.dataengineering.runway.evaluator.schemas
 
 /*
-* Project: Runway
-* Alvaro Muir, Verizon IT Analytics: Data Engineering
-* 04 25, 2018
-*/
+ * Project: Runway
+ * Alvaro Muir, Verizon IT Analytics: Data Engineering
+ * 04 25, 2018
+ */
 
 // http://dmg.org/pmml/v4-3/Taxonomy.html
 trait Taxonomy {
-  case class Taxonomy(name: String,
+  case class Taxonomy(extension: Option[Seq[Extension]] = None,
+                      name: String,
                       childParents: Option[Seq[ChildParent]] = None)
 
-  case class ChildParent(
-      childField: String,
-      parentField: String,
-      parentLevelField: Option[String] = None,
-      isRecursive: String = "false",
-      tableLocator: Option[String],
-      inlineTables: Option[Seq[Row]] = None)
+  case class ChildParent(extension: Option[Seq[Extension]] = None,
+                         childField: String,
+                         parentField: String,
+                         parentLevelField: Option[String] = None,
+                         isRecursive: String = "false",
+                         tableLocator: Option[String],
+                         inlineTables: Option[Seq[Row]] = None)
 
-  case class TableLocator()
+  case class TableLocator(extension: Option[Seq[Extension]] = None)
+
+  case class InlineTable(extension: Option[Seq[Extension]] = None,
+                         row: Option[Seq[Row]])
 
   case class Row(row: String)
 }
