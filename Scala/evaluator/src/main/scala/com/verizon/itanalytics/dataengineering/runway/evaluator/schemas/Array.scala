d76c68f7ead9ca70515dc98a8e749597a0c45300
Muir, Alvaro <alvaro.muir@one.verizon.com>
2018-05-13 07:30:26 -0400

    added sparse int and real components

--- Scala/evaluator/src/main/scala/com/verizon/itanalytics/dataengineering/runway/evaluator/schemas/Array.scala
+++ Scala/evaluator/src/main/scala/com/verizon/itanalytics/dataengineering/runway/evaluator/schemas/Array.scala
@@ -1,13 +1,22 @@
 package com.verizon.itanalytics.dataengineering.runway.evaluator.schemas
 
 /*
-* Project: Runway
-* Alvaro Muir, Verizon IT Analytics: Data Engineering
-* 05 02, 2018
-*/
-
+ * Project: Runway
+ * Alvaro Muir, Verizon IT Analytics: Data Engineering
+ * 05 02, 2018
+ */
 
 trait Array {
   case class Array(n: Int, `type`: String, value: String)
+
+  case class IntSparseArray(indices: Seq[Int],
+                            entries: Seq[Int],
+                            n: Int,
+                            defaultValue: Option[Int] = Option(0))
+
+  case class RealSparseArray(indices: Seq[Double],
+                             entries: Seq[Double],
+                             n: Int,
+                             defaultValue: Option[Double] = Option(0))
 
 }
