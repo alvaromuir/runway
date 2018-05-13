20d918b7448075c383b763f4e7afab55588ac071
Muir, Alvaro <alvaro.muir@one.verizon.com>
2018-05-13 09:52:59 -0400

    completed targets schema

--- Scala/evaluator/src/main/scala/com/verizon/itanalytics/dataengineering/runway/evaluator/schemas/Targets.scala
+++ Scala/evaluator/src/main/scala/com/verizon/itanalytics/dataengineering/runway/evaluator/schemas/Targets.scala
@@ -21,9 +21,48 @@ trait Targets {
   )
 
   case class TargetValue(
+      extension: Option[Seq[Extension]] = None,
+      partition: Option[Seq[Partition]] = None,
       value: Option[String] = None,
       displayValue: Option[String] = None,
       priorProbability: Option[Double] = None,
       defaultValue: Option[Double]
+  )
+
+  case class Partition(extension: Option[Seq[Extension]] = None,
+                       partitionFieldStats: Option[Seq[PartitionFieldStats]] =
+                         None,
+                       name: String,
+                       number: Option[Double] = None)
+
+  case class PartitionFieldStats(
+      extension: Option[Seq[Extension]] = None,
+      counts: Option[Counts] = None,
+      numericInfo: Option[NumericInfo] = None,
+      frequenciesType: Option[FrequenciesType] = None,
+      field: String,
+      weighted: Option[Int] = Some(0))
+
+  case class Counts(extension: Option[Seq[Extension]] = None,
+                    totalFreq: Double,
+                    missingFreq: Option[Double] = None,
+                    invalidFreq: Option[Double] = None,
+                    cardinality: Option[Int] = None)
+
+  case class NumericInfo(extension: Option[Seq[Extension]] = None,
+                         quantile: Option[Quantile] = None,
+                         minimum: Option[Double] = None,
+                         maximum: Option[Double] = None,
+                         mean: Option[Double] = None,
+                         standardDeviation: Option[Double] = None,
+                         median: Option[Double] = None,
+                         interQuartileRange: Option[Double] = None)
+
+  case class Quantile(extension: Option[Seq[Extension]] = None,
+                      quantileLimit: Double,
+                      quantileValue: Double)
+
+  case class FrequenciesType(
+      numArray: Array
   )
 }
