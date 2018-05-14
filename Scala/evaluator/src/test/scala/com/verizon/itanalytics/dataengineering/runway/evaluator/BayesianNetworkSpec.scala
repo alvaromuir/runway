2e555a202bc48b1887cae006f9bcbd383d3b43e7
Muir, Alvaro <alvaro.muir@one.verizon.com>
2018-05-13 22:43:35 -0400

    removed unused code

--- Scala/evaluator/src/test/scala/com/verizon/itanalytics/dataengineering/runway/evaluator/BayesianNetworkSpec.scala
+++ Scala/evaluator/src/test/scala/com/verizon/itanalytics/dataengineering/runway/evaluator/BayesianNetworkSpec.scala
@@ -3,15 +3,12 @@ package com.verizon.itanalytics.dataengineering.runway.evaluator
 import org.scalatest.FlatSpec
 
 /*
-* Project: Runway
-* Alvaro Muir, Verizon IT Analytics: Data Engineering
-* 04 26, 2018
-*/
+ * Project: Runway
+ * Alvaro Muir, Verizon IT Analytics: Data Engineering
+ * 04 26, 2018
+ */
 
-class BayesianNetworkSpec
-    extends FlatSpec
-    with Builder
-    with Evaluator {
+class BayesianNetworkSpec extends FlatSpec with Builder with Evaluator {
 
   "the evaluator" should
     "read Bayesian Network models" in {
