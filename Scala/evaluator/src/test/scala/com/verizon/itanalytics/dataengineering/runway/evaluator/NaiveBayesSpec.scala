19971075d59bdb2af769190ea28b38e4ad3bb281
Muir, Alvaro <alvaro.muir@one.verizon.com>
2018-05-13 22:44:56 -0400

    removed unused code

--- Scala/evaluator/src/test/scala/com/verizon/itanalytics/dataengineering/runway/evaluator/NaiveBayesSpec.scala
+++ Scala/evaluator/src/test/scala/com/verizon/itanalytics/dataengineering/runway/evaluator/NaiveBayesSpec.scala
@@ -12,24 +12,12 @@ import org.scalatest.FlatSpec
  * 05 05, 2018
  */
 
-class NaiveBayesSpec
-    extends FlatSpec
-    with Builder
-    with Evaluator {
-
-//  val testModelPath = mapModels("naiveBayes")
-//  val pMML: PMML = readPMML(new File(testModelPath))
-//  val evaluator: ModelEvaluator[_ <: Model] = evaluatePmml(pMML)
-//  val pmmlModel: PMMLSchema = parsePmml(evaluator.getPMML)
-//  val model: Option[NaiveBayesModel] = pmmlModel.naiveBayesModel
+class NaiveBayesSpec extends FlatSpec with Builder with Evaluator {
 
   "the evaluator" should
     "read Naive Bayes models" in {
     val status = "not yet implemented, requires a test file"
 
     assert(status != null)
-//    val modelClass = "Naive Bayes model"
-//    assert(evaluator.getSummary == modelClass)
-
   }
 }
