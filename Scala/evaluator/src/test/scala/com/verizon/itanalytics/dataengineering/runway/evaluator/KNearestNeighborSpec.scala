4bf6395d5ad301fe37702065c64a5b4e792d0c4c
Muir, Alvaro <alvaro.muir@one.verizon.com>
2018-05-13 22:44:33 -0400

    removed unused code

--- Scala/evaluator/src/test/scala/com/verizon/itanalytics/dataengineering/runway/evaluator/KNearestNeighborSpec.scala
+++ Scala/evaluator/src/test/scala/com/verizon/itanalytics/dataengineering/runway/evaluator/KNearestNeighborSpec.scala
@@ -3,29 +3,17 @@ package com.verizon.itanalytics.dataengineering.runway.evaluator
 import org.scalatest.FlatSpec
 
 /*
-* Project: Runway
-* Alvaro Muir, Verizon IT Analytics: Data Engineering
-* 05 05, 2018
-*/
+ * Project: Runway
+ * Alvaro Muir, Verizon IT Analytics: Data Engineering
+ * 05 05, 2018
+ */
 
-class KNearestNeighborSpec
-  extends FlatSpec
-    with Builder
-    with Evaluator {
-
-  //  val testModelPath = mapModels("nearestNeighbor")
-  //  val testDataPath = mapData("nearestNeighbor")
-  //  val pMML: PMML = readPMML(new File(testModelPath))
-  //  val evaluator: ModelEvaluator[_ <: Model] = evaluatePmml(pMML)
-  //  val pmmlModel: PMMLSchema = parsePmml(evaluator.getPMML)
-  //  val model: Option[BayesianNetworkModel] = pmmlModel.bayesianNetworkModel
+class KNearestNeighborSpec extends FlatSpec with Builder with Evaluator {
 
   "the evaluator" should
     "read Nearest neighbor models" in {
     val status = "not yet implemented, requires a test file"
 
     assert(status != null)
-    //    val modelClass = "Nearest neighbor model"
-    //    assert(evaluator.getSummary == modelClass)
   }
 }
