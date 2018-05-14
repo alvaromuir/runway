fbbe8fbfd555556939c23387126ecd38c2873240
Muir, Alvaro <alvaro.muir@one.verizon.com>
2018-05-13 22:43:27 -0400

    removed unused code

--- Scala/evaluator/src/test/scala/com/verizon/itanalytics/dataengineering/runway/evaluator/BaselineModelSpec.scala
+++ Scala/evaluator/src/test/scala/com/verizon/itanalytics/dataengineering/runway/evaluator/BaselineModelSpec.scala
@@ -3,30 +3,18 @@ package com.verizon.itanalytics.dataengineering.runway.evaluator
 import org.scalatest.FlatSpec
 
 /*
-* Project: Runway
-* Alvaro Muir, Verizon IT Analytics: Data Engineering
-* 04 25, 2018
-*/
+ * Project: Runway
+ * Alvaro Muir, Verizon IT Analytics: Data Engineering
+ * 04 25, 2018
+ */
 
-class BaselineModelSpec
-  extends FlatSpec
-    with Builder
-    with Evaluator {
-
-  //  val testModelPath = mapModels("baseline")
-  //  val testDataPath = mapData("baseline")
-  //  val pMML: PMML = readPMML(new File(testModelPath))
-  //  val evaluator: ModelEvaluator[_ <: Model] = evaluatePmml(pMML)
-  //  val pmmlModel: PMMLSchema = parsePmml(evaluator.getPMML)
-  //  val model: Option[BayesianNetworkModel] = pmmlModel.bayesianNetworkModel
+class BaselineModelSpec extends FlatSpec with Builder with Evaluator {
 
   "the evaluator" should
     "read Baseline models" in {
     val status = "not yet implemented, requires a test file"
 
     assert(status != null)
-    //    val modelClass = "Baseline network model"
-    //    assert(evaluator.getSummary == modelClass)
   }
 
 }
