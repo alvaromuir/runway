7e81cc4d7491a3417b5bdc08a5b4517a9167dea7
Muir, Alvaro <alvaro.muir@one.verizon.com>
2018-05-13 22:42:54 -0400

    removed unused code

--- Scala/evaluator/src/test/scala/com/verizon/itanalytics/dataengineering/runway/evaluator/BayesianNetworkSpec.scala
+++ Scala/evaluator/src/test/scala/com/verizon/itanalytics/dataengineering/runway/evaluator/BayesianNetworkSpec.scala
@@ -1,9 +1,5 @@
 package com.verizon.itanalytics.dataengineering.runway.evaluator
 
-import java.io.File
-
-import org.dmg.pmml.{Model, PMML}
-import org.jpmml.evaluator.{ModelEvaluator, UnsupportedElementException}
 import org.scalatest.FlatSpec
 
 /*
@@ -17,20 +13,11 @@ class BayesianNetworkSpec
     with Builder
     with Evaluator {
 
-//  val testModelPath = mapModels("bayesianNetwork")
-//  val testDataPath = mapData("bayesianNetwork")
-//  val pMML: PMML = readPMML(new File(testModelPath))
-//  val evaluator: ModelEvaluator[_ <: Model] = evaluatePmml(pMML)
-//  val pmmlModel: PMMLSchema = parsePmml(evaluator.getPMML)
-//  val model: Option[BayesianNetworkModel] = pmmlModel.bayesianNetworkModel
-
   "the evaluator" should
     "read Bayesian Network models" in {
     val status = "not yet implemented, requires a test file"
 
     assert(status != null)
-    //    val modelClass = "Bayesian network model"
-    //    assert(evaluator.getSummary == modelClass)
   }
 
 }
