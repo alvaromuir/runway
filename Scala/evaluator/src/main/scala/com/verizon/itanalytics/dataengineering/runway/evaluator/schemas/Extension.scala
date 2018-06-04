f82a45103f55c513179eb220944d2f7e0ef6077d
Muir, Alvaro <alvaro.muir@one.verizon.com>
2018-06-04 12:02:34 -0400

    added json formatting functionality

--- Scala/evaluator/src/main/scala/com/verizon/itanalytics/dataengineering/runway/evaluator/schemas/Extension.scala
+++ Scala/evaluator/src/main/scala/com/verizon/itanalytics/dataengineering/runway/evaluator/schemas/Extension.scala
@@ -1,5 +1,7 @@
 package com.verizon.itanalytics.dataengineering.runway.evaluator.schemas
 
+import spray.json._
+
 /*
  * Project: Runway
  * Alvaro Muir, Verizon IT Analytics: Data Engineering
@@ -14,5 +16,15 @@ trait Extension {
       value: Option[String] = None,
       content: Option[Seq[String]] = None
   )
+
+  implicit object ExtensionFormat extends JsonFormat[Extension] {
+    def write(extension: Extension) = JsObject(
+      extension.extender match { case _ => "extender" -> JsString(extension.extender.get) },
+      extension.name match { case _ => "name" -> JsString(extension.name.get) },
+      extension.value match { case _ => "value" -> JsString(extension.value.get) },
+      extension.content match { case _ => "content" -> JsArray(extension.content.get.map(JsString(_)).toVector) }
+    )
+    def read(json: JsValue) = null // not implemented
+  }
 
 }
