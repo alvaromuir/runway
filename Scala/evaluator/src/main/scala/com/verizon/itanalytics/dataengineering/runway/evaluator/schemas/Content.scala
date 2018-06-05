c993fd4b80d5dc8e806882d7f335f6ca026a0c0b
Muir, Alvaro <alvaro.muir@one.verizon.com>
2018-06-05 13:43:54 -0400

    added json conversion

--- Scala/evaluator/src/main/scala/com/verizon/itanalytics/dataengineering/runway/evaluator/schemas/Content.scala
+++ Scala/evaluator/src/main/scala/com/verizon/itanalytics/dataengineering/runway/evaluator/schemas/Content.scala
@@ -1,5 +1,7 @@
 package com.verizon.itanalytics.dataengineering.runway.evaluator.schemas
 
+import spray.json._
+
 /*
 * Project: Runway
 * Alvaro Muir, Verizon IT Analytics: Data Engineering
@@ -8,10 +10,26 @@ package com.verizon.itanalytics.dataengineering.runway.evaluator.schemas
 
 
 trait Content {
-  case class Content(locator: Option[Locator] = None)
-
   case class Locator(publicId: Option[String] = None,
                      systemId: Option[String] = None,
                      lineNumber: Int = 1,
                      columnNumber: Int = 1)
+  case class Content(locator: Option[Locator] = None)
+
+  implicit object LocatorFormat extends JsonFormat[Locator] {
+    def write(locator: Locator) = JsObject(
+      locator.publicId match { case _ => "publicId" -> JsString(locator.publicId.get) },
+      locator.systemId match { case _ => "systemId" -> JsString(locator.systemId.get) },
+      "lineNumber" -> JsNumber(locator.lineNumber),
+      "columnNumber" -> JsNumber(locator.columnNumber)
+    )
+    def read(json: JsValue): Null = null // not implemented
+  }
+
+  implicit object ContentFormat extends JsonFormat[Content] {
+    def write(content: Content) = JsObject(
+      content.locator match { case _ => "publicId" -> content.locator.toJson }
+    )
+    def read(json: JsValue): Null = null // not implemented
+  }
 }
