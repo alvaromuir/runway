fb6becb849dbfc2942d6ad7cff247a7eb3f5fc00
Muir, Alvaro <alvaro.muir@one.verizon.com>
2018-06-06 11:40:05 -0400

    fixed typo

--- Scala/evaluator/src/main/scala/com/verizon/itanalytics/dataengineering/runway/evaluator/models/AssociationRules.scala
+++ Scala/evaluator/src/main/scala/com/verizon/itanalytics/dataengineering/runway/evaluator/models/AssociationRules.scala
@@ -218,7 +218,7 @@ trait AssociationRules extends AssociationModel {
                                                 )
                                               })
                                       },
-                                    totalValueSom =
+                                    totalValueSum =
                                       u.getContStats.getTotalValuesSum,
                                     totalSquaresSum =
                                       u.getContStats.getTotalSquaresSum
