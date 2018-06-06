66c252b80c06cfae33e80b71f341d809d09499b0
Muir, Alvaro <alvaro.muir@one.verizon.com>
2018-06-06 11:37:02 -0400

    added json conversion

--- Scala/evaluator/src/main/scala/com/verizon/itanalytics/dataengineering/runway/evaluator/schemas/DerivedField.scala
+++ Scala/evaluator/src/main/scala/com/verizon/itanalytics/dataengineering/runway/evaluator/schemas/DerivedField.scala
@@ -1,5 +1,7 @@
 package com.verizon.itanalytics.dataengineering.runway.evaluator.schemas
 
+import spray.json.{DefaultJsonProtocol, RootJsonFormat}
+
 /*
  * Project: Runway
  * Alvaro Muir, Verizon IT Analytics: Data Engineering
@@ -13,4 +15,10 @@ trait DerivedField extends DataDictionary {
                           dataType: String,
                           intervals: Option[Seq[Interval]] = None,
                           values: Option[Seq[String]] = None)
+
+  implicit object DerivedFieldProtocol extends DefaultJsonProtocol {
+    implicit val arrayFormat: RootJsonFormat[DerivedField] =  jsonFormat6(DerivedField)
+  }
 }
+
+
